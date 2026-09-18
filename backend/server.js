d0d0a94c165fa9508a762773a8fd58a664348c59
Amanul Haque <growth.amanul@gmail.com>
2026-09-18 15:12:28 +0530

    Income Backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
+import incomeRouter from './routes/incomeRoute.js';
 
 const app = express();
 const port = 4000;
@@ -20,6 +21,7 @@ connectDB();
 
 //ROUTES
 app.use("/api/user", userRouter)
+app.use("/api/income", incomeRouter)
 
 app.get('/',(req,res) =>{
     res.send("API WORKING");
